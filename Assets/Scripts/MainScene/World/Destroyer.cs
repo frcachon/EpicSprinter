42ac95c3a59d310a72dc2ae775930490f1b7785d
frcachon <fracachon@gmail.com>
2023-11-15 23:33:23 +0000

    periodic destruction of clones

---
+++ Assets/Scripts/MainScene/World/Destroyer.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyer : MonoBehaviour {
+
+    // this script destroys the infinitely and randomly generated
+    // clones of world in order to make the game more efficient
+
+    public string parentName;
+
+    void Start() {
+        parentName = transform.name;
+        StartCoroutine(DestroyClone());
+    }
+
+    IEnumerator DestroyClone() {
+        yield return new WaitForSeconds(50);
+        if (parentName == "World(Clone)")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
